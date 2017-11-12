426b6a15053ed35dfb225e54aef43a82cb1b8241
John C <jcresswell@hotmail.com>
2017-11-12 07:25:26 -0600

    begin implementing repository

--- SperroFunctions/ActiveGames.cs
+++ SperroFunctions/ActiveGames.cs
@@ -4,6 +4,9 @@ using System.Net.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using SperroFunctions.Models;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace SperroFunctions
 {
@@ -14,8 +17,19 @@ namespace SperroFunctions
         {
             log.Info("C# HTTP trigger function processed a request.");
 
+            // TEMP: active games object
+            var activeGames = new List<Game>();
+
+            activeGames.Add(new Game()
+            {
+                Id = 1,
+                Title = "Random Run",
+                Description = "Total randomness",
+                Publisher = new GamePublisher()
+            });
+
             // Fetching the name from the path parameter in the request URL
-            return req.CreateResponse(HttpStatusCode.OK, "Hello from activegames");
+            return req.CreateResponse(HttpStatusCode.OK, JsonConvert.SerializeObject(activeGames));
         }
     }
 }
---
+++ SperroFunctions/Interfaces/IGameRepository.cs
@@ -0,0 +1,16 @@
+﻿using SperroFunctions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SperroFunctions.Interfaces
+{
+    interface IGameRepository : IRepository<Game, int>
+    {
+        IEnumerable<Game> FindAll();
+
+        IEnumerable<Game> FindActve();
+    }
+}
