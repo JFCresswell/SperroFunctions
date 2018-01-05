1541ebec77880bcc6fcd35973e55223dfeba2bc7
John Cresswell <jcresswell@hotmail.com>
2018-01-04 22:13:35 -0600

    response objects

---
+++ SperroFunctions/Responses/GameResponse.cs
@@ -0,0 +1,21 @@
+﻿using SperroFunctions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SperroFunctions.Responses
+{
+    public class GameResponse : Response
+    {
+        // consider temporary token
+        public int Id { get; set; } 
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public GamePublisher Publisher { get; set; }
+    }
+}
---
+++ SperroFunctions/Responses/SponsorResponse.cs
@@ -0,0 +1,18 @@
+﻿using SperroFunctions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SperroFunctions.Responses
+{
+    public class SponsorResponse : Response
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IList<Prize> Prizes { get; private set; }
+    }
+}
